d448c7d81cdd1900665092cc11c304202c8feb60
Navaneethakrshnan <navaneethak381@gmail.com>
2024-03-19 18:11:41 +0530

    Edit code

--- crud.js
+++ crud.js
@@ -16,10 +16,40 @@ function readAll() {
       (elements += `<tr>
       <td>${record.name}</td>
       <td>${record.email}</td>
-      <td><button>Edit</button>
-      <button>Delete</button></td>
+      <td>
+      <button class="edit" onclick={edit(${record.id})}>Edit</button>
+      <button class="delete">Delete</button></td>
       </tr>`)
   );
 
   tabledata.innerHTML = elements;
+}
+
+function create() {
+  document.querySelector(".create_form").style.display = "block";
+  document.querySelector(".add_div").style.display = "none";
+}
+
+function Add_data() {
+  var name = document.querySelector(".name").value;
+  var email = document.querySelector(".email").value;
+
+  var newObj = {
+    id: 3,
+    name: name,
+    email: email,
+  };
+  data.push(newObj);
+
+  document.querySelector(".create_form").style.display = "none";
+  document.querySelector(".add_div").style.display = "block";
+
+  readAll();
+}
+
+function edit(id) {
+  document.querySelector(".update_form").style.display = "block";
+  var obj = data.find((rec) => rec.id === id);
+  document.querySelector(".uname").value = obj.name;
+  document.querySelector(".uemail").value = obj.email;
 }
